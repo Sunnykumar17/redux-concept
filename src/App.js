92d36c761007678d6ee1470990c9f41dd1856b3d
Sunny kumar <98548654+Sunnykumar17@users.noreply.github.com>
2023-04-14 16:44:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InputComp from './components/InputComp';
+
+function App() {
+  return (
+    <div className="App">
+      <InputComp/>
+    </div>
+  );
+}
+
+export default App;
